b004669e13eb8cfb6080a4ac0ffa550f235eec21
Shashank Mukherjee <mastermindshaan@users.noreply.github.com>
2024-02-14 02:02:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Container from 'react-bootstrap/Container';
 import UserInputList from './UserInputList';
 import "./App.css";
-const getUserLoginAPIUrl = "https://api-server-user.vercel.app/user";
+const getUserLoginAPIUrl = "https://user-demo-vercel-server.vercel.app/user";
 const App = () => {
   return (
     <Container>
